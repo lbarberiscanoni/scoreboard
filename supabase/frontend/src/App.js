c6ead58e4e5012f62275d4ae62a4134c5dd452b8
lbarberiscanoni <hllbck7@gmail.com>
2025-02-24 14:37:40 -0800

    removed the cal component now that the cal tracker is its own separate repo

--- supabase/frontend/src/App.js
+++ supabase/frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EventChart from './components/EventChart';
-import CalendarClient from './components/Cal';
-import Cal from './components/Cal';
 
 function App() {
   return (
@@ -10,7 +8,6 @@ function App() {
       <Routes>
         {/* Dynamic routes for different organizations and input types */}
         <Route path="/:organization/:inputType" element={<EventChart />} />
-        <Route path="/cal" element={<Cal />} />
         {/* Default or fallback route */}
         <Route path="/" element={<h1>Welcome to the Dashboard</h1>} />
       </Routes>
